9e1eb1f66346d00c608011ef46dc6498fe951ed1
Miguel Gargallo <mgargallo16@gmail.com>
2024-02-02 19:11:13 +0100

    Update nuevo.lua

--- nuevo.lua
+++ nuevo.lua
@@ -1,4 +1,4 @@
--- Author: yakuzadeso, modified by miguelgargallo
+-- Author: yakuzadeso
 
 local config = {}
 --------------------------------------------------------------------------------
@@ -8,63 +8,62 @@ local config = {}
 config.OverWeightSpeedZero_AddPercent = 50.0 -- Default value 50.0. Not tested.
 
 -- Automatic HP regeneration percentage per second
-config.AutoHPRegene_Percent_perSecond = 10.0 -- Was 0.3, Default value 0.3. Not tested.
+config.AutoHPRegene_Percent_perSecond = 0.3 -- Default value 0.3. Not tested.
 
 -- Automatic HP regeneration percentage per second while sleeping
-config.AutoHPRegene_Percent_perSecond_Sleeping = 100.0 -- Was 1.0, Default value 1.0. Not tested.
+config.AutoHPRegene_Percent_perSecond_Sleeping = 1.0 -- Default value 1.0. Not tested.
 
 -- HP decrease percentage per second when starving
-config.Starvation_DecreaseHP_Percent_perSecond = 0.0 -- Was 3.0, Default value 3.0. Not tested.
-
--- Stomach decrease per second for monsters
-config.StomachDecreace_perSecond_Monster = 0.000 -- Was 0.025, Default value 0.025. Not tested.
+config.Starvation_DecreaseHP_Percent_perSecond = 3.0 -- Default value 3.0. Not tested.
 
 -- Stomach decrease per second for players
-config.StomachDecreace_perSecond_Player = 0.00 -- Was 0.05, Default value 0.05. Not tested.
+config.StomachDecreace_perSecond_Player = 0.05 -- Default value 0.05. Not tested.
 
 -- Stomach decrease for auto-healing
-config.StomachDecreace_AutoHealing = 2.0 -- Default value 2.0. Not tested.
+config.StomachDecreace_AutoHealing = 0.0 -- Default value 2.0. Not tested.
 
 -- Stomach decrease for working activities
-config.StomachDecreace_WorkingRate = 2.0 -- Default value 2.0. Not tested.
+config.StomachDecreace_WorkingRate = 0.0 -- Default value 2.0. Not tested.
 
 -- Initial stomach value when hungry
-config.HungerStart_StomachValue = 30 -- Default value 30. Not tested.
+config.HungerStart_StomachValue = 20 -- Default value 30. Not tested.
 
 -- Percentage of health to have after respawn
-config.PlayerHPRateFromRespawn = 0.5 -- Default 0.5. Looks like percentage so should go up to 1.0 (100)
+config.PlayerHPRateFromRespawn = 1 -- Default 0.5. Looks like percentage so should go up to 1.0 (100)
 
 -- Percentage of hunger meter after respawn
-config.PlayerStomachRateFromRespawn = 0.8 -- Default 0.8. Looks like percentage so should go up to 1.0 (100)
+config.PlayerStomachRateFromRespawn = 1 -- Default 0.8. Looks like percentage so should go up to 1.0 (100)
 
 -- Stamina recovery percentage per second
-config.StaminaRecover_PercentPerSecond = 50.0 -- Was 20, Default value 20.0. Not tested.
+config.StaminaRecover_PercentPerSecond = 80.0 -- Default value 20.0. Not tested.
 
 -- Percentage of health to trigger low health effect
 config.lowHealthEffectParcent = 50 -- Default value 50. Not tested.
 
 -- Wait time to respawn
-config.BlockRespawnTime = 2.5 -- Was 5.0, Default value 5.0. Lowest value you can set is 0.1 which is essentially instant respawns.
+config.BlockRespawnTime = 5.0 -- Default value 5.0. Lowest value you can set is 0.1 which is essentially instant respawns.
 
 -- Default starting inventory weight
 config.DefaultMaxInventoryWeight = 300.0 -- Default value 300.0.
 
 -- Default starting Technology points
-config.DefaultTechnologyPoint = 5 -- Was 0, Default value 0.
+config.DefaultTechnologyPoint = 23 -- Default value 0.
 
+-- Seconds to stay in revive state?
+config.RemainderOfLife_Second = 300 -- Default value 300. Not tested.
 
 
 --------------------------------------------------------------------------------
 -- Technology points settings
 
 -- Technology points gained per tower boss?
-config.bossTechnologyPointPerTowerBoss = 10 -- Was 5.
+config.bossTechnologyPointPerTowerBoss = 15 -- Default value 5.
 
 -- Technology points gained per normal boss?
-config.bossTechnologyPointPerNormalBoss = 5 -- Was 1.
+config.bossTechnologyPointPerNormalBoss = 5 -- Default value 1.
 
 -- Technology points gained per fast travel point?
-config.TechnologyPoint_UnlockFastTravel = 2 -- Was 1.
+config.TechnologyPoint_UnlockFastTravel = 5 -- Default value 1. Not tested.
 
 
 --------------------------------------------------------------------------------
@@ -104,7 +103,7 @@ config.AddWorkSpeedPerStatusPoint = 50.0 -- Default value 50.0. Not tested.
 --AddWorkSpeedPerWorkSpeedRank = 0.03 -- Default value 0.03. Not tested.
 
 -- Additional technology points to gain per level
-config.technologyPointPerLevel = 6 -- Default value 6. Not tested.
+config.technologyPointPerLevel = 18 -- Default value 6. Not tested.
 
 
 --------------------------------------------------------------------------------
@@ -112,110 +111,91 @@ config.technologyPointPerLevel = 6 -- Default value 6. Not tested.
 -- Movement settings
 
 -- Max walking speed
-config.MaxWalkSpeed = 700.0 -- Was 350.0, Default value 350.0. Not tested.
+config.MaxWalkSpeed = 500.0 -- Default value 350.0.
 
 -- Max sprinting speed
-config.SprintMaxSpeed = 1000.0 -- Was 500.0, Default value 500.0. Not tested.
+config.SprintMaxSpeed = 500.0 -- Default value 500.0.
 
 -- Max climbing speed
-config.ClimbMaxSpeed = 200.0 -- Was 100.0, Default value 100.0. Not tested.
+config.ClimbMaxSpeed = 500.0 -- Default value 100.0.
 
 -- Max swimming speed
-config.MaxSwimSpeed = 500.0 -- Was 250.0, Default value 250.0. Not tested.
+config.MaxSwimSpeed = 500.0 -- Default value 250.0.
 
 -- Max dash swimming speed
-config.DashSwimMaxSpeed = 1000.0 -- Was 500.0, Default value 500.0. Not tested.
-
---
-config.JumpableInWaterDepth = 30.0 -- Default value 30.0. Not tested.
-
---
-config.FatigueMaxSpeed = 100.0 -- Default value 100.0. Not tested.
-
---
-config.AirControl = 0.2 -- Default value 0.2. Not tested.
-
---
-config.GliderMaxSpeed = 350.0 -- Default value 350.0. Not tested.
-
---
-config.GliderAirControl = 1.0 -- Default value 1.0. Not tested.
-
---
-config.GliderGravityScale = 0.015 -- Default value 0.015. Not tested.
-
+config.DashSwimMaxSpeed = 750.0 -- Default value 500.0.
 
 --------------------------------------------------------------------------------
 
 -- Stamina Settings
 
 -- Climbing stamina consumption for movement
-config.ClimbingStamina_Move = 1.0 -- Was 10, Default value 10.0. Not tested.
+config.ClimbingStamina_Move = 0.0 -- Default value 10.0.
 
 -- Climbing stamina consumption for jumping
-config.ClimbingStamina_Jump = 1.0 -- Was 10, Default value 10.0. Not tested.
+config.ClimbingStamina_Jump = 0.0 -- Default value 10.0.
 
 -- Stamina rate for ride waza
-config.RideWazaStaminaRate = 0.3 -- Default value 0.3. Not tested.
+config.RideWazaStaminaRate = 0.0 -- Default value 0.3.
 
 -- Stamina consumption for flying in hover mode
-config.FlyHover_SP = 0.1 -- Default value 0.1. Not tested.
+config.FlyHover_SP = 0.0 -- Default value 0.1.
 
 -- Stamina for flying horizontally
-config.FlyHorizon_SP = 1.0 -- Was 2.5, Default value 2.5. Not tested.
+config.FlyHorizon_SP = 0.0 -- Default value 2.5.
 
 -- Stamina for dashing in horizontal flying
-config.FlyHorizon_Dash_SP = 1.0 -- Was 10.0, Default value 10.0. Not tested.
+config.FlyHorizon_Dash_SP = 0.0 -- Default value 10.0.
 
 -- Stamina for flying vertically
-config.FlyVertical_SP = 1.0 -- Was 5.0, Default value 5.0. Not tested.
+config.FlyVertical_SP = 0.0 -- Default value 5.0. 
 
 -- Stamina for jumping
-config.JumpSP = 1 -- Was 10, Default value 10.
+config.JumpSP = 0.0 -- Default value 10.
 
 -- Stamina for melee attacks
-config.MeleeAttackSP = 1.0 -- Was 5, Default value 5.
+config.MeleeAttackSP = 0.0 -- Default value 5.
 
 -- Stamina for sprinting
-config.SprintSP = 1.0 -- Was 7.0, Default value 7.0.
+config.SprintSP = 0.0 -- Default value 7.0.
 
 -- Stamina for gliding
-config.GliderSP = 1.0 -- Was 15.0, Default value 15.0. Not tested.
+config.GliderSP = 0.0 -- Default value 15.0.
 
 -- Stamina for idle swimming
-config.Swimming_SP_Idle = 0.1 -- Was 1.5, Default value 1.5. Not tested.
+config.Swimming_SP_Idle = 0.0 -- Default value 1.5.
 
 -- Stamina for regular swimming
-config.Swimming_SP_Swim = 0.1 -- Was 1.5, Default value 1.5. Not tested.
+config.Swimming_SP_Swim = 0.0 -- Default value 1.5.
 
 -- Stamina for dashing while swimming
-config.Swimming_SP_DashSwim = 0.1 -- Was 3.0, Default value 3.0. Not tested.
+config.Swimming_SP_DashSwim = 0.0 -- Default value 3.0.
 
 
 --------------------------------------------------------------------------------
 
 -- Base camp settings
 
--- Max area range considered base camp?
-config.BaseCampAreaRange = 4500.0 -- Was 3500.0, Default value 3500.0. Not tested.
+-- Max area range considered base camp. Need to dismantle palbox and rebuild it to take effect.
+config.BaseCampAreaRange = 5000.0 -- Default value 3500.0.
 
--- Max range in which pals can find work?
-config.BaseCampPalFindWorkRange = 4500.0 -- Was 400.0., Default value 400.0. Not tested.
+-- Max range in which pals can find work
+config.BaseCampPalFindWorkRange = 5000.0 -- Default value 400.0.
 
--- Max range to find workable foliage?
-config.BaseCampFoliageWorkableRange = 4500.0 -- Was 200.0, Default value 200.0. Not tested.
+-- Max range to find workable foliage
+config.BaseCampFoliageWorkableRange = 5000.0 -- Default value 200.0.
 
 -- ???????????????????????
-config.WorkAmountBySecForPlayer = 100.0 -- Default value 100.0. Not tested.
+config.WorkAmountBySecForPlayer = 5000.0 -- Default value 100.0. Not tested.
 
--- Max range of the pal box?
-config.palboxAreaRange = 4500.0 -- Was 1500.0, Default value 1500.0. Not tested.
+-- Max range of the pal box. Need to dismantle and rebuild it to take effect.
+config.palboxAreaRange = 5000.0 -- Default value 1500.0.
 
 -- ??????????????????????
-config.BaseCampAreaRangeStorage = 5000.0 -- Default value 5000.0. Not tested.
+config.BaseCampAreaRangeStorage = 5000.0 -- Default value 5000.0.
 
 -- Max slots in camp chests?
-config.ChestSlotNum = 250 -- Was 100, Default value 100. Not tested.
+config.ChestSlotNum = 100 -- Default value 100. Not tested.
 
 -- Max resource stacks for workers?
 config.WorkerCollectResourceStackMaxNum = 10 -- Default value 10. Not tested.
@@ -227,11 +207,10 @@ config.FarmCropGrowupSpeedBySec = 1.0 -- Default value 1.0. Not tested.
 config.FarmCropIncreaseRateByWaterFillRate = 3.0 -- Default value 3.0. Not tested.
 
 -- Probability for camp invansions?
-config.InvadeProbability = 0.2 -- Was 0.8, Default value 0.8. Not tested.
+config.InvadeProbability = 0.0 -- Default value 0.8. Not tested.
 
 -- Probability for npc visitations?
-config.VisitorNPCProbability = 0.2 -- Was 0.5, Default value 0.5. Not tested.
-
+config.VisitorNPCProbability = 0.0 -- Default value 0.5. Not tested.
 
 
 --------------------------------------------------------------------------------
@@ -268,6 +247,11 @@ config.BossOrRarePal_TalentMin = 50 -- Default 50. Not tested.
 -- Should be the time you need to wait before reviving pals.
 config.PalBoxReviveTime = 10.0 -- Default value 10.0. Not tested.
 
+-- Sanity auto regen in pal storage
+config.AutoSANRegene_Percent_perSecond_PalStorage = 0.02 -- Default value 0.02. Not tested.
+
+-- Stomach decrease per second for monsters
+config.StomachDecreace_perSecond_Monster = 0.025 -- Default value 0.025. Not tested.
 
 --------------------------------------------------------------------------------
 
